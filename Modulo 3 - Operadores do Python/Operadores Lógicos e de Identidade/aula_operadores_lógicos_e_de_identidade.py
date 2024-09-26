ea079de88ecbbbe1cdc91daf9b8bf0a635315ce7
Adriano Saadeh <adrianosaadeh@gmail.com>
2024-09-26 08:27:38 -0300

    mod 3

---
+++ Modulo 3 - Operadores do Python/Operadores Lógicos e de Identidade/aula_operadores_lógicos_e_de_identidade.py
@@ -0,0 +1,16 @@
+"""Operadores Lógicos = Operadores Booleanos: not, and, or"""
+print(True and True)
+print(True and False)
+print(False and False)
+print(False or False)
+print(True or False)
+
+print(not True)
+print(not False)
+print(4 > 6 and 10 <= 15)
+
+"""Operadores de Identidade: is, is not"""
+print(10 is 8)
+print(10 == 8)
+print(6 is not 8)
+print(6 != 8)
---
+++ Modulo 3 - Operadores do Python/Operadores de Comparação/aula_operadores_de_comparação.py
@@ -0,0 +1,14 @@
+"""Aula - Operadores de Comparação"""
+
+print(5 == 10 / 2)
+
+print(5 != 5)
+# neste caso é como se eu perguntasse se 5 é diferente de 5 e ai é falso pois são iguais
+
+print(5 != 3)
+
+print(500 < 200)
+
+print(5 >= 10 + 6)
+
+print(15 <= 10 + 6)
